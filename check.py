9201cddb71f01417dd8b020bf1ac51118b7852ad
amalsebs <48290044+amalsebs@users.noreply.github.com>
2021-01-03 11:28:24 +0530

    Create check.py

---
+++ check.py
@@ -0,0 +1,14 @@
+import time
+from fabric import Connection
+from civo import Civo
+
+ip_server = civo.instances.search(filter='hostname:{}'.format('csi-agrohealthhack'))[0]['public_ip']
+username = 'admin'
+
+c = Connection('{}@{}'.format(username, ip_server))
+result = c.put('webroot.gz', remote='/tmp')
+print("Uploaded {0.local} to {0.remote}".format(result))
+c.sudo('apt update')
+c.sudo('apt install -qy nginx')
+c.sudo('rm -rf /var/www/html/*')
+c.sudo('tar -C /var/www/html/ -xzvf /tmp/webroot.gz')
